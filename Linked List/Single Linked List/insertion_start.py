6ca6f0fd1abd07e3206ab08595bd27fbc0ba684a
Tanmay Srivastava <tanmay@getinfino.com>
2020-06-28 17:01:17 +0530

    feat: insertions (Start, Pos), Traversal

---
+++ Linked List/Single Linked List/insertion_start.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertData(self, data):
+        newNode = Node(data)
+        newNode.next = self.head
+        self.head = newNode
+    
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+if __name__ == "__main__":
+    list = LinkedList()
+    list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    list.head.next = second
+    second.next = third
+
+    list.insertData(1)
+    list.insertData(2)
+    list.insertData(3)
+    list.printList()
---
+++ Linked List/Single Linked List/traversal.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+if __name__ == "__main__":
+    list = LinkedList()
+    list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    fourth = Node(4)
+    fifth = Node(5)
+
+    list.head.next = second
+    second.next = third
+    third.next = fourth
+    fourth.next = fifth
+
+    list.printList()
